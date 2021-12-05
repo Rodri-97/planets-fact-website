5f112a44b13cc8f27121102e282d7636ca859748
rodrigo <rodrigo_coelho@orange.fr>
2021-12-05 19:21:16 +0100

    set up mobile selector

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -3,7 +3,14 @@ import "../styles/Planet.css";
 const Planet = (props) => {
     const { planet } = props;
     return (
-        <div className="planet page">{planet.name}</div>
+        <div className="planet page">
+            <div className="selector selector-top">
+                <div className="selector-item selector-top-item">Overview</div>
+                <div className="selector-item selector-top-item">Internal</div>
+                <div className="selector-item selector-top-item">Surface</div>
+            </div>
+            {planet.name}
+        </div>
     )
 }
 
