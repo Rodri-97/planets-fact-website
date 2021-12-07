0357b2690be6395e156a8bdb4e81ebb80c605790
rodrigo <rodrigo_coelho@orange.fr>
2021-12-07 17:56:28 +0100

    style tabs

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -5,6 +5,9 @@ import "../styles/Planet.css";
 const Planet = (props) => {
     const { currentPlanet } = props;
     const [currentTab, setCurrentTab] = useState("Overview");
+    const styles = {
+        borderBottom: `4px solid ${currentPlanet.color}`
+    }
 
     return (
         <div className="planet page">
@@ -12,11 +15,24 @@ const Planet = (props) => {
                 <div 
                     className="selector-item selector-top-item" 
                     onClick={() => setCurrentTab("Overview")}
+                    style={currentTab === "Overview" ? styles : null}
                 >
                     Overview
                 </div>
-                <div className="selector-item selector-top-item" onClick={() => setCurrentTab("Internal")}>Internal</div>
-                <div className="selector-item selector-top-item" onClick={() => setCurrentTab("Surface")}>Surface</div>
+                <div 
+                    className="selector-item selector-top-item" 
+                    onClick={() => setCurrentTab("Internal")}
+                    style={currentTab === "Internal" ? styles : null}
+                >
+                        Internal
+                </div>
+                <div 
+                    className="selector-item selector-top-item" 
+                    onClick={() => setCurrentTab("Surface")}
+                    style={currentTab === "Surface" ? styles : null}
+                >
+                    Surface
+                </div>
             </div>
             <Tab currentPlanet={currentPlanet} currentTab={currentTab}/>
         </div>
